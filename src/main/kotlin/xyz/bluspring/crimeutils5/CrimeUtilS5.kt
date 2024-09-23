3053c7b1bd512de435df69bb99010126cb9a145e
Naz <bluspringindohq@gmail.com>
2024-09-23 08:13:09 +0800

    Initial stuff + mipmap fixes

---
+++ src/main/kotlin/xyz/bluspring/crimeutils5/CrimeUtilS5.kt
@@ -0,0 +1,13 @@
+package xyz.bluspring.crimeutils5
+
+import net.fabricmc.api.ModInitializer
+
+class CrimeUtilS5 : ModInitializer {
+
+    override fun onInitialize() {
+    }
+
+    companion object {
+        const val MOD_ID = "crimecraft"
+    }
+}
---
+++ src/main/kotlin/xyz/bluspring/crimeutils5/client/CrimeUtilS5Client.kt
@@ -0,0 +1,9 @@
+package xyz.bluspring.crimeutils5.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class CrimeUtilS5Client : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
