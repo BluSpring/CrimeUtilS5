61e346fe778e56fa47a2931831971a95eb441388
Naz <bluspringindohq@gmail.com>
2024-09-30 21:17:30 +0800

    more useful workaround stuff

--- src/main/java/xyz/bluspring/crimeutils5/mixin/MappedRegistryMixin.java
+++ src/main/java/xyz/bluspring/crimeutils5/mixin/MappedRegistryMixin.java
@@ -1,20 +1,56 @@
 package xyz.bluspring.crimeutils5.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.core.Holder;
-import net.minecraft.core.MappedRegistry;
-import net.minecraft.core.RegistrationInfo;
+import com.mojang.serialization.Lifecycle;
+import it.unimi.dsi.fastutil.objects.ObjectList;
+import net.minecraft.core.*;
 import net.minecraft.resources.ResourceKey;
-import org.spongepowered.asm.mixin.Mixin;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.bluspring.crimeutils5.Workarounds;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Mixin(MappedRegistry.class)
 public class MappedRegistryMixin<T> {
+    @Mutable
+    @Shadow @Final private ObjectList<Holder.Reference<T>> byId;
+
+    @Shadow @Nullable private Map<T, Holder.Reference<T>> unregisteredIntrusiveHolders;
+    @Unique private Map<Holder.Reference<T>, Throwable> fuckOff = new HashMap<>();
+
+    @Inject(method = "<init>(Lnet/minecraft/resources/ResourceKey;Lcom/mojang/serialization/Lifecycle;Z)V", at = @At("TAIL"))
+    private void useForcedTracingMap(ResourceKey<T> key, Lifecycle registryLifecycle, boolean hasIntrusiveHolders, CallbackInfo ci) {
+        this.byId = new Workarounds.ForcedTracedObjectMap<>(256);
+    }
+
     @Inject(method = "register", at = @At("TAIL"))
     private void storeIdDump(ResourceKey<T> key, T value, RegistrationInfo registrationInfo, CallbackInfoReturnable<Holder.Reference<T>> cir, @Local int id) {
         Workarounds.INSTANCE.getFuckThatsAwful().put(id, key);
+    }
+
+    @WrapOperation(method = "method_40271", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Holder$Reference;createIntrusive(Lnet/minecraft/core/HolderOwner;Ljava/lang/Object;)Lnet/minecraft/core/Holder$Reference;"))
+    private Holder.Reference<T> detectIntrusiveCrashCall(HolderOwner<T> owner, T value, Operation<Holder.Reference<T>> original) {
+        //noinspection MixinExtrasOperationParameters
+        var call = original.call(owner, value);
+        fuckOff.put(call, new Exception("fuck"));
+
+        return call;
+    }
+
+    @Inject(method = "freeze", at = @At(value = "INVOKE", target = "Ljava/lang/IllegalStateException;<init>(Ljava/lang/String;)V", shift = At.Shift.BEFORE))
+    private void dumpIntrusiveCallers(CallbackInfoReturnable<Registry<T>> cir) {
+        for (Holder.Reference<T> reference : this.unregisteredIntrusiveHolders.values()) {
+            if (this.fuckOff.containsKey(reference)) {
+                this.fuckOff.get(reference).printStackTrace();
+            }
+        }
     }
 }
--- src/main/kotlin/xyz/bluspring/crimeutils5/Workarounds.kt
+++ src/main/kotlin/xyz/bluspring/crimeutils5/Workarounds.kt
@@ -1,8 +1,41 @@
 package xyz.bluspring.crimeutils5
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap
+import it.unimi.dsi.fastutil.objects.ObjectArrayList
 import net.minecraft.resources.ResourceKey
 
 object Workarounds {
     val fuckThatsAwful = Int2ObjectArrayMap<ResourceKey<*>>()
+
+    class ForcedTracedObjectMap<T>(capacity: Int) : ObjectArrayList<T>(capacity) {
+        override fun remove(element: T): Boolean {
+            try {
+                throw Exception("calling a test for $element")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            return super.remove(element)
+        }
+
+        override fun removeAt(index: Int): T {
+            return super.removeAt(index).apply {
+                try {
+                    throw Exception("removing $index with value $this")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        override fun set(index: Int, element: T): T {
+            try {
+                throw Exception("storing $index with $element")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            return super.set(index, element)
+        }
+    }
 }
