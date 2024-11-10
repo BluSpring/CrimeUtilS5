75f96de13df3be0496997e56916f1f1406c64edd
Naz <bluspringindohq@gmail.com>
2024-11-10 11:47:41 +0800

    Actually print internal exceptions (I THOUGHT THIS WAS DONE ALREADY???)

---
+++ src/main/java/xyz/bluspring/crimeutils5/mixin/ConnectionMixin.java
@@ -0,0 +1,17 @@
+package xyz.bluspring.crimeutils5.mixin;
+
+
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.network.Connection;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Connection.class)
+public class ConnectionMixin {
+    @Inject(method = "exceptionCaught", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;translatable(Ljava/lang/String;[Ljava/lang/Object;)Lnet/minecraft/network/chat/MutableComponent;"))
+    private void printExceptionStackTrace(ChannelHandlerContext channelHandlerContext, Throwable throwable, CallbackInfo ci) {
+        throwable.printStackTrace();
+    }
+}
